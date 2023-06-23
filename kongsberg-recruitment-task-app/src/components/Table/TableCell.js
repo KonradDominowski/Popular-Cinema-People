6b2533ac7e2118f70fa8e98b81f6eda56f869473
Konrad Dominowski <zehkan@gmail.com>
2023-06-23 17:42:32 +0200

    refactor

--- kongsberg-recruitment-task-app/src/components/Table/TableCell.js
+++
@@ -1,5 +0,0 @@
-import classes from './TableCell.module.css'
-
-export default function TableCell({ children, className }) {
-	return <div className={ `${classes.cell} ${className ? classes[className] : ''}` }>{ children }</div>
-}
