86bef2071716607531db58e5deaff1793afee68f
Konrad Dominowski <zehkan@gmail.com>
2023-06-26 18:44:22 +0200

    small alt tweak

--- src/components/UI/Image.js
+++ src/components/UI/Image.js
@@ -2,13 +2,13 @@ import { Suspense } from "react";
 import { useImage } from "react-image";
 import imagePlaceholder from './../../media/imagePlaceholder.jpg'
 
-export default function Image({ imgSrc }) {
+export default function Image({ imgSrc, title }) {
 	const sources = [imgSrc, imagePlaceholder]
 	const { src } = useImage({
 		srcList: sources,
 	})
 
 	return <Suspense>
-		<img src={ src } />
+		<img src={ src } alt={ title ? `${title} poster` : '' } />
 	</Suspense>
 }
